1c8fcd1951f34eb498d70833e70b3f6b83420074
tkql0 <96943505+tkql0@users.noreply.github.com>
2024-11-13 17:39:56 +0900

    [Chore] 맵에 오브젝트를 생성하기 위한 테스트 저장용

--- Assets/1.Scripts/Object/ObjectMove.cs
+++ Assets/1.Scripts/Object/ObjectMove.cs
@@ -9,9 +9,10 @@ public class ObjectMove : MonoBehaviour
     private float speed = 1;
 
     private void OnEnable()
-    {
+    { // 여기를 안거치네 // 아 이미 생성되있는 오브젝트라 위치가 이상한거구나
         _targetPosition = new Vector3(-transform.position.x,
             transform.position.y, transform.position.z);
+        Debug.Log($"{_targetPosition.x} + {_targetPosition.z}");
     }
 
     private void Update()
@@ -20,6 +21,8 @@ public class ObjectMove : MonoBehaviour
         {
             gameObject.SetActive(false);
         }
+
+        //transform.Translate(speed * Time.deltaTime, 0, 0);
     }
 
     private void FixedUpdate()
---
+++ Assets/1.Scripts/Object/SpawnController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public bool goLeft = false;
+    public bool goRight = false;
+
+    public List<GameObject> items;
+    public Spawner spawnersLeft;
+    public Spawner spawnersRight;
+}
